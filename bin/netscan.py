cbcb2767549994822475ccb00b29cfd5aec42cb5
Raghuram Subramani <raghus2247@gmail.com>
2021-05-21 03:42:59 -0400

    Added all functionality and intuitive interface

--- bin/netscan.py
+++ bin/netscan.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
-#imports
+#Imports
 import scapy.all as scapy
+import re
+from mac_vendor_lookup import MacLookup
+from scapy.data import PPI_CAPTURE_INFO
 
-#Functions
+#Function to scan ip addresses
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
@@ -16,11 +19,72 @@ def scan(ip):
         clients_list.append(clients_dict)
     return clients_list
 
-def print_result(results_list):
-    print("IP\t\t\tMAC_Address\n--------------------------------------------------------")
-    for client in results_list:
-        print(client["ip"] + "\t\t" + client["mac"])
+mac = MacLookup()
+
+print('''
+███╗░░██╗███████╗████████╗░██████╗░█████╗░░█████╗░███╗░░██╗
+████╗░██║██╔════╝╚══██╔══╝██╔════╝██╔══██╗██╔══██╗████╗░██║
+██╔██╗██║█████╗░░░░░██║░░░╚█████╗░██║░░╚═╝███████║██╔██╗██║
+██║╚████║██╔══╝░░░░░██║░░░░╚═══██╗██║░░██╗██╔══██║██║╚████║
+██║░╚███║███████╗░░░██║░░░██████╔╝╚█████╔╝██║░░██║██║░╚███║
+╚═╝░░╚══╝╚══════╝░░░╚═╝░░░╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝░░╚══╝''')
+
+print("\n")
 
 #Main
-scan_result = scan('10.10.10.0/24')
-print_result(scan_result)
+choice = input("Would you like to:\n\n1. Scan an IP address\n\n2. Scan an IP range\n\n3. Update vendor sources (Requires Wifi)\n\n> ")
+
+#Pattern to check if IP range is valid
+range_pattern = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]*$")
+
+#Pattern to check if IP is valid
+ip_pattern = re.compile("^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$")
+
+if choice == "1":
+    print('''
+██╗██████╗░  ░█████╗░██████╗░██████╗░██████╗░███████╗░██████╗░██████╗
+██║██╔══██╗  ██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+██║██████╔╝  ███████║██║░░██║██║░░██║██████╔╝█████╗░░╚█████╗░╚█████╗░
+██║██╔═══╝░  ██╔══██║██║░░██║██║░░██║██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██║██║░░░░░  ██║░░██║██████╔╝██████╔╝██║░░██║███████╗██████╔╝██████╔╝
+╚═╝╚═╝░░░░░  ╚═╝░░╚═╝╚═════╝░╚═════╝░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░''')
+    ip_address_chosen = input("\nEnter an IP address > ")
+    if ip_pattern.search(ip_address_chosen):
+        print("\nIP is valid, script can continue...\n")
+    else:
+        print("IP is not valid! Script will exit now...")
+        exit(0)
+    scan_result = scan(ip_address_chosen)
+    print("IP" + "\t\t\t" + "MAC" + "\t\t\t" +"Vendor")
+    for client in scan_result:
+        print("------------------------------------------------------------------")
+        print(client["ip"] + "\t\t" + client["mac"] + "\t" + MacLookup().lookup(client["mac"]))
+elif choice == "2":
+    print('''
+██╗██████╗░  ██████╗░░█████╗░███╗░░██╗░██████╗░███████╗
+██║██╔══██╗  ██╔══██╗██╔══██╗████╗░██║██╔════╝░██╔════╝
+██║██████╔╝  ██████╔╝███████║██╔██╗██║██║░░██╗░█████╗░░
+██║██╔═══╝░  ██╔══██╗██╔══██║██║╚████║██║░░╚██╗██╔══╝░░
+██║██║░░░░░  ██║░░██║██║░░██║██║░╚███║╚██████╔╝███████╗
+╚═╝╚═╝░░░░░  ╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝░╚═════╝░╚══════╝''')
+    range_chosen = input("\nEnter an IP Range > ")
+    if range_pattern.search(range_chosen):
+        print("\nIP Range is valid, script can continue...\n")
+    else:
+        print("IP Range is not valid! Script will exit now...")
+        exit(0)
+    scan_result = scan(range_chosen)
+    print("IP" + "\t\t\t" + "MAC" + "\t\t\t" + "Vendor")
+    for client in scan_result:
+        print("------------------------------------------------------------------")
+        print(client["ip"] + "\t\t" + client["mac"] + "\t" + MacLookup().lookup(client["mac"]))
+elif choice == "3":
+    print("\nUpdating sources... This can take a few seconds...\n")
+    mac = MacLookup()
+    mac.update_vendors()
+    print("Done!")
+
+    def find_mac(mac_address):
+        print(mac.lookup(mac_address))
+else:
+    print("Thats not a valid input, Please chose either 1 or 2!")
